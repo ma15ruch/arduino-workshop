e5d2da52dd2a4eaeb803cecdf07b6c696ed7b279
ma15ruch <ruchi.maheshwari2010@yahoo.com>
2014-07-11 14:50:48 -0700

    Create follower.java

---
+++ follower.java
@@ -0,0 +1,21 @@
+import zen.core.Zen;
+import zen.game.Sprite;
+
+
+public class Follower extends Sprite{
+
+	public void draw() {
+		Zen.setColor("turquoise");
+		Zen.fillOval(getX(), getY(), 30, 30);
+	}
+
+	public void move() {
+		
+	}
+
+	public void setup() {
+		
+	}
+
+}
+
