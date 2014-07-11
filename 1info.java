c65b3251cd0acebb2ac293f335a73905e3e849ee
ma15ruch <ruchi.maheshwari2010@yahoo.com>
2014-07-11 15:15:33 -0700

    Update 1info.java

--- 1info.java
+++ 1info.java
@@ -1,2 +1,12 @@
 import zenlab from zenlab.io (this is the url) 
 import zenlab and firebase and use eclipse!
+
+TO DOs:
+
+bouncyBallObjects => gravity
+Cannon => score
+ListFun => biggest number
+FlappyBird => randomize gates
+PrimeNumbers => get all prime numbers up to 100
+tag => make score
+CTF2 =>finish game with flag and score
