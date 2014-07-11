1a133afa71aab0f0287352b00e7aa044acc98a25
ma15ruch <ruchi.maheshwari2010@yahoo.com>
2014-07-11 15:45:46 -0700

    Update 1info.java

--- 1info.java
+++ 1info.java
@@ -10,3 +10,5 @@ FlappyBird => randomize gates
 PrimeNumbers => get all prime numbers up to 100
 tag => make score
 CTF2 =>finish game with flag and score
+
+print out the fibonachi sequence (1,1,2,3,5,8,13,21,34,55, etc........)
