7023640c61e5f24877a9b268a893ecf08705ff08
ma15ruch <ruchi.maheshwari2010@yahoo.com>
2014-07-11 14:52:45 -0700

    Create MadLib.java

---
+++ MadLib.java
@@ -0,0 +1,27 @@
+
+
+public class MadLib 
+{
+
+	
+	public static void main(String[] args) 
+	{
+		String word;
+		word = "toilet";
+		String word2;
+		word2 = "shopping";
+		String word3;
+		word3 = "toothbrushes";
+		String word4;
+		word4 = "toothpaste";
+		String word5;
+		word5 = "biking";
+		
+		System.out.println("Hello, my dear" + " " + word + ".");
+		System.out.println("Would you like to go" + " " + word2 + " with me?");
+		System.out.println("I need to buy " + word3 + " " + "and " + word4 + ".");
+		System.out.println("If you don't want to come with me " + word + ", " + "I can go " + word5 + " instead.");
+
+	}
+
+}
